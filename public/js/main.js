3f124d77af4b9f4870b19d9578199b71f3189c0e
eggsmayhem <flipmybit@protonmail.com>
2022-09-05 13:23:56 -0400

    working, without ability to refresh

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,50 @@
-function geoFindMe() {
+// document.querySelector('#find-me').addEventListener('click', geoFindMe);
+//if that asks users permission, determines if they are geolocator compatible, and returns coords into DOM 
+(() => {
+  const message = document.querySelector('#message');
 
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
-    function success(position) {
-      const latitude  = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-  
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-  
-    if (!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-  
+  // const map = null
+
+  // check if the Geolocation API is supported
+  if (!navigator.geolocation) {
+      message.textContent = `Your browser doesn't support Geolocation`;
+      message.classList.add('error');
+      return;
   }
-  
-  document.querySelector('#find-me').addEventListener('click', geoFindMe);
-  
+
+  // handle click event
+  const btn = document.querySelector('#find-me');
+  btn.addEventListener('click', function () {
+      // get the current position
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  });
+
+
+  // handle success case
+  function onSuccess(position) {
+      const {
+          latitude,
+          longitude
+      } = position.coords;
+
+      message.classList.add('success');
+      // remove a map if there already is one so a new map can be intialized
+      const voidCheck = L.DomUtil.get('map')
+      if (voidCheck != null) {
+        voidCheck._leafleat_id = null
+      }
+     
+      const map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13)
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
+
+      message.textContent = `Your location: (${latitude},${longitude})`;
+  }
+
+  // handle error case
+  function onError() {
+      message.classList.add('error');
+      message.textContent = `Failed to get your location!`;
+  }
+})();
